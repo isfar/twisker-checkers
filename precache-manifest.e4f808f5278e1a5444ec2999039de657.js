9ef16741d456874cfd7336713a5ee97d2beb7af5
isfar <isfar.sifat@gmail.com>
2019-10-01 20:51:17 +0600

    Updates

---
+++ precache-manifest.e4f808f5278e1a5444ec2999039de657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562feb68f026cb930000942bef249aaf",
+    "url": "/twisker-checkers/index.html"
+  },
+  {
+    "revision": "8b63cdd177a3bd7b8ffd",
+    "url": "/twisker-checkers/static/css/main.76b019a9.chunk.css"
+  },
+  {
+    "revision": "2adc85e22ae6314ca73d",
+    "url": "/twisker-checkers/static/js/2.5aba6afd.chunk.js"
+  },
+  {
+    "revision": "8b63cdd177a3bd7b8ffd",
+    "url": "/twisker-checkers/static/js/main.4a6bacf4.chunk.js"
+  },
+  {
+    "revision": "385cac5ac7cd97dd02e1",
+    "url": "/twisker-checkers/static/js/runtime-main.cdd873fc.js"
+  },
+  {
+    "revision": "87581c70d77e825512ef2d83e851a472",
+    "url": "/twisker-checkers/static/media/crown.87581c70.png"
+  }
+]);
